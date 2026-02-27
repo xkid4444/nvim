f57880cea602cbeea1d52ef29a6048c97fea253b
xkid4444 <xkid4444@gmail.com>
2026-02-27 23:21:58 +1100

    treesitter custom syntax install added

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -7,7 +7,26 @@
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
 
+-- turn off spelling checks for txt & log files
 vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
   pattern = { "*.txt", "*.log" },
   command = "setlocal nospell",
+})
+
+-- log tree-sitter
+vim.api.nvim_create_autocmd("User", {
+  pattern = "TSUpdate",
+  callback = function()
+    require("nvim-treesitter.parsers").log = {
+      install_info = {
+        -- change path as need (try to use full paths)
+        path = "~/parsers/tree-sitter-log",
+        -- optional entries
+        location = "parser",
+        generate = true,
+        generate_from_json = false,
+        -- queries = "queries/neovim", -- symlink queries from given directory
+      },
+    }
+  end,
 })
